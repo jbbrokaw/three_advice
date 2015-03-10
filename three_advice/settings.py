8336e18ce49c0fe40cb14370eca7215d7efef0a3
Jason Brokaw <jbrokaw@gmail.com>
2015-03-10 13:17:22 -0700

    Database setup

--- three_advice/settings.py
+++ three_advice/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'three_advice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'threes',
+        'USER': 'threes',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '15432',
     }
 }
 
